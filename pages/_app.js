26b8e2bfc9d24c6ff0722e33aceb9fbb50032cae
raya <raya@rhp-valuation.com>
2022-07-04 15:28:50 +0700

    fix bootstrap.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
   return <Component {...pageProps} />
 }
 
